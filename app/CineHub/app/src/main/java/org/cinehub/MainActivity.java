e6f3027d2d4ffed681253961bdc792e6d94a0d65
alexeses <46347629+alexeses@users.noreply.github.com>
2023-03-07 19:00:15 +0100

    declare basic elements and assing setOnclickListener to buttons

--- app/CineHub/app/src/main/java/org/cinehub/MainActivity.java
+++ app/CineHub/app/src/main/java/org/cinehub/MainActivity.java
@@ -3,12 +3,35 @@ package org.cinehub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declare the variables
+    private EditText et_username;
+    private EditText et_password;
+    private Button btn_login;
+    private Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the variables
+        et_username = findViewById(R.id.et_username);
+        et_password = findViewById(R.id.et_passwd);
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
+
+        btn_login.setOnClickListener(v -> {
+            // TODO: Login
+        });
+
+        btn_register.setOnClickListener(v -> {
+            // TODO: Register
+        });
+
     }
 }
