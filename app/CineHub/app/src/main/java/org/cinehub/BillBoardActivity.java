794e63b5d4030d1c3905130a8891c7800f4fa535
alexeses <46347629+alexeses@users.noreply.github.com>
2023-03-10 17:54:33 +0100

    added billboard activity

---
+++ app/CineHub/app/src/main/java/org/cinehub/BillBoardActivity.java
@@ -0,0 +1,14 @@
+package org.cinehub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BillBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill_board);
+    }
+}
