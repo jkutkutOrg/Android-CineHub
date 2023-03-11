c22a1a1589e825602993bdc7fc2efedee969428f
alexaib <alexaib@outlook.es>
2023-03-12 00:50:18 +0100

    Simplified intent data collection

--- app/CineHub/app/src/main/java/org/cinehub/NavActivity.java
+++ app/CineHub/app/src/main/java/org/cinehub/NavActivity.java
@@ -11,18 +11,12 @@ import java.util.function.Supplier;
 Describes an Activity which allows moving forward while preserving its intent data
  */
 public abstract class NavActivity extends AppCompatActivity {
-    /*
-    This method should be overridden, so it can insert the current's activity intent data
-    into the intent to be launched on 'advanceActivity'
-     */
-    @NonNull
-    protected abstract Intent collectData(@NonNull Intent intent);
 
     /*
     intentSupplier should be a lambda defined on the caller, which returns the 'default' intent
     with the current activity data about to be launched
      */
     protected void advanceActivity(@NonNull Supplier<Intent> intentSupplier) {
-        startActivity(collectData(intentSupplier.get()));
+        startActivity(intentSupplier.get().putExtras(getIntent()));
     }
 }
