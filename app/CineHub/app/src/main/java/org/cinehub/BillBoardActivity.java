d1954030e62afe324e93aadc7279c168fef0e439
alexaib <alexaib@outlook.es>
2023-03-12 00:30:10 +0100

    Implemented base NavActivity methods on BillBoardActivity

--- app/CineHub/app/src/main/java/org/cinehub/BillBoardActivity.java
+++ app/CineHub/app/src/main/java/org/cinehub/BillBoardActivity.java
@@ -1,17 +1,21 @@
 package org.cinehub;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import org.cinehub.api.model.Movie;
+import org.cinehub.api.model.User;
 import org.cinehub.utils.MovieAdapter;
 
 import java.util.ArrayList;
 
-public class BillBoardActivity extends AppCompatActivity {
+public class BillBoardActivity extends NavActivity {
+
+    public static final String EXTRA_MOVIE = "Movie";
 
     private MovieAdapter mvAdapter;
     private RecyclerView rvMovies;
@@ -34,5 +38,18 @@ public class BillBoardActivity extends AppCompatActivity {
 
         mvAdapter = new MovieAdapter(movieList);
         rvMovies.setAdapter(mvAdapter);
+
+        // TODO implement once Rv listeners are done
+//        advanceActivity(() -> new Intent(this, SeatSelectionActivity.class)
+//                .putExtra(EXTRA_MOVIE, new Movie()));
+    }
+
+
+    @NonNull
+    @Override
+    protected Intent collectData(@NonNull Intent intent) {
+        return intent
+                .putExtra(LoginActivity.EXTRA_USER, (User) getIntent()
+                        .getParcelableExtra(LoginActivity.EXTRA_USER));
     }
 }
