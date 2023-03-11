1d77c316ba9c0adaded4993bc5785172910baf7b
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-03-11 16:44:33 +0100

    now able to test api with tests

---
+++ app/CineHub/app/src/androidTest/java/org/cinehub/api/SignupTest.java
@@ -0,0 +1,82 @@
+package org.cinehub.api;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class SignupTest {
+
+    public static final int VALID = 0;
+    public static final int INVALID = 1;
+
+    static CinehubAuth auth;
+    static CinehubDB db;
+
+    AtomicInteger running;
+
+    @BeforeClass
+    public static void setup() {
+        // Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        // assertEquals("org.cinehub", appContext.getPackageName());
+
+        auth = CinehubAPI.getAuthInstance();
+        db = CinehubAPI.getDBInstance();
+    }
+
+    @Before
+    public void setupTest() {
+        running = new AtomicInteger(1);
+    }
+
+    @After
+    public void teardownTest() {
+        while (running.get() == 1) ;
+        assertEquals(0, running.get());
+        running.set(-1);
+    }
+
+    @Test
+    public void signup() {
+        auth.signup(
+            "test",
+            "testmail@mail.com",
+            "test1234",
+            () -> {
+                running.set(VALID);
+            },
+            e -> {
+                running.set(INVALID);
+            }
+        );
+    }
+
+    @Test
+    public void signupError() {
+        auth.signup(
+            "Marta",
+            "marta@gmail.com",
+            "martamarta",
+            () -> {
+                running.set(INVALID);
+            },
+            e -> {
+                running.set(VALID);
+            }
+        );
+    }
+}
