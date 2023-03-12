6e89581e0d26d2eebce7eb8d349a7d9a80bd3bff
alexeses <46347629+alexeses@users.noreply.github.com>
2023-03-12 19:07:55 +0100

    Added Auth wihth db and clear fields on exit

--- app/CineHub/app/src/main/java/org/cinehub/LoginActivity.java
+++ app/CineHub/app/src/main/java/org/cinehub/LoginActivity.java
@@ -1,36 +1,43 @@
 package org.cinehub;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.cinehub.api.CinehubAPI;
+import org.cinehub.api.CinehubAuth;
 import org.cinehub.utils.UserValidationUtils;
 
 public class LoginActivity extends AppCompatActivity {
 
-    // Declare the variables
     private EditText etEmail;
     private EditText etPassword;
     private Button btnLogin;
     private Button btnRegister;
+
+    private CinehubAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Initialize the variables
         etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPasswd);
         btnLogin = findViewById(R.id.btnLogic);
         btnRegister = findViewById(R.id.btnRegister);
 
+        // Initialize CinehubAuth instance
+        auth = CinehubAPI.getAuthInstance();
+
         btnLogin.setOnClickListener(v -> {
             String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
 
             boolean isValid = true;
 
@@ -39,15 +46,36 @@ public class LoginActivity extends AppCompatActivity {
                 isValid = false;
             }
 
-            if (isValid) {
-                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show();
+            if (password.isEmpty()) {
+                Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                isValid = false;
             }
 
+            if (isValid) {
+                auth.login(email, password, this::onLoginSuccess, this::onLoginError);
+            }
         });
 
         btnRegister.setOnClickListener(v -> {
+            clearFields();
             startActivity(new Intent(this, RegisterActivity.class));
         });
-
     }
+
+    private void clearFields() {
+        etEmail.setText("");
+        etPassword.setText("");
+    }
+
+    private void onLoginSuccess() {
+        Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show();
+        //startActivity(new Intent(this, BillBoardActivity.class));
+        finish();
+    }
+
+    private void onLoginError(@NonNull String error) {
+        Toast.makeText(this, "Error: " + error, Toast.LENGTH_SHORT).show();
+        System.out.println("Error: " + error);
+    }
+
 }
