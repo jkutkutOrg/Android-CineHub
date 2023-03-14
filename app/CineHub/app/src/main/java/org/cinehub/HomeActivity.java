fbf6690e7fa801e9161722165e706009621fdec2
alexaib <alexaib@outlook.es>
2023-03-14 12:42:17 +0100

    Home activity basic data displayed

--- app/CineHub/app/src/main/java/org/cinehub/HomeActivity.java
+++ app/CineHub/app/src/main/java/org/cinehub/HomeActivity.java
@@ -1,6 +1,10 @@
 package org.cinehub;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.cinehub.api.CinehubAPI;
+import org.cinehub.api.model.User;
 
 public class HomeActivity extends NavActivity {
 
@@ -8,5 +12,14 @@ public class HomeActivity extends NavActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        TextView tvHomeTitle = findViewById(R.id.tvHomeTitle);
+        TextView tvHomeSub = findViewById(R.id.tvHomeSub);
+
+        User user = getIntent().getParcelableExtra(LoginActivity.EXTRA_USER);
+        CinehubAPI api = new CinehubAPI();
+
+        tvHomeTitle.setText(getString(R.string.title_home, user.getName()));
+        // TODO implement reservation view
     }
 }
