815cf99f8627b6c000764d91b5fd8b6b88b4d72c
alexaib <alexaib@outlook.es>
2023-03-14 11:20:46 +0100

    Home activity added

---
+++ app/CineHub/app/src/main/java/org/cinehub/HomeActivity.java
@@ -0,0 +1,12 @@
+package org.cinehub;
+
+import android.os.Bundle;
+
+public class HomeActivity extends NavActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
