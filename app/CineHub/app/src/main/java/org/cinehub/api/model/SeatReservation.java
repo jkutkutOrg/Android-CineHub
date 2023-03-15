bdb0c41c71e6a5bd254018a23fca06c85f961824
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-03-15 08:47:53 +0100

    Using the seat class when nor a reservation nor a configuration seat is needed

--- app/CineHub/app/src/main/java/org/cinehub/api/model/SeatReservation.java
+++ app/CineHub/app/src/main/java/org/cinehub/api/model/SeatReservation.java
@@ -19,10 +19,6 @@ public class SeatReservation extends Seat implements Parcelable {
 
     public SeatReservation() {}
 
-    public SeatReservation(int row, int col) {
-        this(-1, row, col, -1);
-    }
-
     public SeatReservation(int projection, int row, int col, int reservation) {
         super(row, col);
         setProjection(projection);
